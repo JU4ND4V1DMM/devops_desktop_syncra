31d7d6817be103e324ce922a2766ddeb92f0adbe
JU4ND4V1DMM <ju4n.d4v1d.m3nd3z@gmail.com>
2025-04-18 19:39:50 -0500

    Dynamic read to get disk with most free space to Pyspark Files Temps

--- web/pyspark.py
+++ web/pyspark.py
@@ -1,18 +1,42 @@
 from pyspark.sql import SparkSession
 import os
+import shutil
+import string
+
+def get_disk_with_most_free_space():
+    
+    best_drive = None
+    max_free = 0
+    
+    for drive_letter in string.ascii_uppercase:
+        drive = f"{drive_letter}:/"
+        if os.path.exists(drive):
+            try:
+                total, used, free = shutil.disk_usage(drive)
+                if free > max_free:
+                    max_free = free
+                    best_drive = drive
+            except PermissionError:
+                continue
+            
+    return best_drive
 
 def get_spark_session():
     
     print("Initializing Spark Session...")
-    
+
     spark = SparkSession.builder \
         .appName("GlobalSparkApp") \
         .config("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false") \
         .getOrCreate()
+
+    best_disk = get_disk_with_most_free_space()
+    temp_dir = os.path.join(best_disk, "SparkTemp")
     
-    temp_dir = "C:/SparkTemp"  # Path secure to store temporary files
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
     os.makedirs(temp_dir, exist_ok=True)
-    
+
     spark_2 = SparkSession \
             .builder.appName("GlobalSparkApp_Config") \
             .config("spark.local.dir", temp_dir) \
