54b81c59f2a4c13ce32b708545c3513cac27eef2
JU4ND4V1DMM <ju4n.d4v1d.m3nd3z@gmail.com>
2025-04-18 19:28:14 -0500

    Update to start session with pyspark to adjust Java´s memory and folder of files temp

--- web/pyspark.py
+++ web/pyspark.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+import os
 
 def get_spark_session():
     
@@ -8,16 +9,20 @@ def get_spark_session():
         .appName("GlobalSparkApp") \
         .config("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false") \
         .getOrCreate()
-        
-    spark_2 = SparkSession \
-            .builder.appName("BD_CN") \
-            .config("spark.local.dir", "C:/tmp/hive") \
-            .config("spark.driver.extraJavaOptions", "-Djava.security.manager=allow") \
-            .config("spark.executor.extraJavaOptions", "-Djava.security.manager=allow") \
-            .config("spark.driver.memory", '16g')\
-            .config("spark.executor.memory", '16g')\
-            .getOrCreate()
     
-    spark_2.conf.set("mapreduce.fileoutputcomitter.marksuccessfuljobs","false")
+    temp_dir = "C:/SparkTemp"  # Path secure to store temporary files
+    os.makedirs(temp_dir, exist_ok=True)
+    
+    spark_2 = SparkSession \
+            .builder.appName("GlobalSparkApp_Config") \
+            .config("spark.local.dir", temp_dir) \
+            .config("spark.driver.memory", "16g") \
+            .config("spark.executor.memory", "16g") \
+            .config("spark.driver.maxResultSize", "4g") \
+            .config("spark.sql.shuffle.partitions", "50") \
+            .config("spark.driver.extraJavaOptions", "-XX:+UseG1GC -Djava.security.manager=allow") \
+            .config("spark.executor.extraJavaOptions", "-XX:+UseG1GC -Djava.security.manager=allow") \
+            .config("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false") \
+            .getOrCreate()
         
     return spark_2
